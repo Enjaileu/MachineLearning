254c838eda1d5d847e3d360f5c3841fa7263eaa0
SIONNEAU Angèle <angele.sionneau@artfx34.lan>
2023-02-01 17:37:40 +0100

    add cmments

--- MachineLearning/Main.cpp
+++ MachineLearning/Main.cpp
@@ -7,13 +7,7 @@
 
 using namespace std;
 
-/// <summary>
-/// calculates the predicted values (or predicted targets) for a given set of inputs and thetas.
-/// </summary>
-/// <param name="x">param x : inputs(feature values) - Numpy array of floats</param>
-/// <param name="t0">param theta_0 : intercept - float</param>
-/// <param name="t1">param theta_1 : slope - float</param>
-/// <returns>predicted targets - Numpy array of floats</returns>
+
 float hypothesis(float x, float t0, float t1) {
 	return t1 * x + t0;
 }
@@ -33,42 +27,27 @@ double cost(map<int, float>& content, float t0, float t1 ) {
 }
 
 void gradient(map<int, float>& content, float& t0, float& t1, float step) {
-	/*
-	float totalErrorDiff0{0.f};
-	float totalErrorDiff1{0.f};
-	int dataSize{ int(content.size()) };
 
-	for (auto i = content.begin(); i != content.end(); i++) {
-		float hypoY{ hypothesis(i->first, t0, t1) };
-		const float errorDiff0{ hypoY - i->second };
-		totalErrorDiff0 += errorDiff0;
-		const float errorDiff1{ (hypoY - i->second) * i->first };
-		totalErrorDiff1 += errorDiff1;
-	}
-
-	float temp0 = (step / dataSize) * totalErrorDiff0;
-	float temp1 = (step / dataSize) * totalErrorDiff1;
-
-	t0 -= temp0;
-	t1 -= temp1;
-
-	*/
-
-	//float totalError0{ 0.f };
+	float totalError0{ 0.f };
 	float totalError1{ 0.f };
 	int dataSize{ int(content.size()) };
 
+	//loop training sur notre dataset
 	for (auto i = content.begin(); i != content.end(); i++) {
+		//hypothese
 		float hypo{ hypothesis(i->first, t0, t1) };
-		//const float errorDiff{ hypo - i->second };
+		// differences
+		const float errorDiff{ hypo - i->second };
 		const float errorDiff1 =  (hypo - i->second) * i->first;
-		//totalError0 += errorDiff;
+		//on augmente les totaux
+		totalError0 += errorDiff;
 		totalError1 += errorDiff1;
 	}
 
-	//float temp0 = (step / dataSize) * totalError0;
+	// assigne nouvelles valeurs aux thetas
+	float temp0 = (step / dataSize) * totalError0;
 	float temp1 = (step / dataSize) * totalError1;
-	//t0 -= temp0;
+	t0 -= temp0;
 	t1 -= temp1;
 }
 
@@ -97,13 +76,14 @@ void findThetas(float &t0, float &t1, float stop, float step, map<int, float> &c
 		cout << "New cost = " << newCost << endl;
 		cout << endl;
 		iter++;
-	} while (iter <10);
+	} while (iter <5);
 	//while(diff > stop
 	
 }
 
 int main() {
 
+	//on récupere les datas pour les mettre dans une map
 	string filePath{ "./ressources/test.csv" };
 
 	map<int, float> content{ FileManagement::returnAsIF(filePath) };
